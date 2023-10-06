65ca309390f58c322fe3bbe6f34071321669568c
unknown <steen_hansen@yahoo.com>
2023-10-06 16:50:33 -0700

    heroku try

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phoenix_hello, MultiGameWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
+ url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- lib/multi_game_web/endpoint.ex
+++ lib/multi_game_web/endpoint.ex
@@ -11,7 +11,8 @@ defmodule MultiGameWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  #socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket, websocket: [timeout: 45_000]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
