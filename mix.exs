4598272af0005b16b6cbaf59212855d0c5b8dc7b
unknown <steen_hansen@yahoo.com>
2023-10-06 16:52:12 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MultiGame.MixProject do
     [
       app: :multi_game,
       version: "0.1.0",
-      elixir: "~> 1.9.4",
+      elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
