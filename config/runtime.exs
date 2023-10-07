f2a9a8c9699272c26b828e60d04382ec6e4c16ce
unknown <steen_hansen@yahoo.com>
2023-10-06 19:37:05 -0700

    ....

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,7 +33,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "evening-eyrie-25210-f2edb2eac607.herokuapp.com"
+  host = System.get_env("PHX_HOST") || "evening-eyrie-25210.herokuapp.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :multi_game, MultiGameWeb.Endpoint,
